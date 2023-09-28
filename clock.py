400166b2c027e7a58d5212f337a02554e084a0af
connoromalley <william.omalley@umconnect.umt.edu>
2023-09-27 22:15:24 -0600

    finished clock

--- clock.py
+++ clock.py
@@ -1,2 +1,21 @@
 # clock app from a tiktok i watched
 
+from tkinter import *
+from time import strftime
+
+myWindow = Tk()
+myWindow.title('My Clock')
+
+def time():
+    myTime = strftime('%H:%M:%S %p')
+    clock.config(text = myTime)
+    clock.after(1000, time)
+
+clock = Label(myWindow, font = ('arial', 30, 'bold'),
+                                background = 'green',
+                                foreground = 'white')
+
+clock.pack(anchor = 'center')
+time()
+
+mainloop()
